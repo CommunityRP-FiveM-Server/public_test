08b09c1267dc5d8267938a833a1309d0a3b2803e
Gellipapa <Gellipapa@users.noreply.github.com>
2024-01-20 15:32:04 +0000

    Formatted file:  fxmanifest.lua Formatted file:  main.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,8 +10,8 @@ legacyversion("1.9.1")
 shared_script("@es_extended/imports.lua")
 
 client_scripts({
-			"@es_extended/locale.lua",
-		"locales/*.lua",
+	"@es_extended/locale.lua",
+	"locales/*.lua",
 	"config.lua",
 	"client/main.lua",
 })
