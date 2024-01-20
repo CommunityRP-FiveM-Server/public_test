35ccafe3d4aae9fb07a70f37a1eeef83afe2669c
Gellipapa <gellerthorvath0@gmail.com>
2024-01-20 16:09:07 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,8 +11,8 @@ shared_script("@es_extended/imports.lua")
 
 client_scripts({
 	"@es_extended/locale.lua",
-	"locales/*.lua",
-	"config.lua",
+		"locales/*.lua",
+				"config.lua",
 	"client/main.lua",
 })
 
