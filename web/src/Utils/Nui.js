56384bcff6ba0417394c526ea523e1e94a56d3fa
Gellipapa <gellerthorvath0@gmail.com>
2024-01-20 03:02:48 +0100

    Update Nui.js

--- web/src/Utils/Nui.js
+++ web/src/Utils/Nui.js
@@ -2,10 +2,6 @@ export const Nui = {
 
                 async send(event, data = {}) {
         try {
-                        const resp = await fetch(`https://${GetParentResourceName()}/${event}`, {
-
-            async send(event, data = {}) {
-        try {
                     const resp = await fetch(`https://${GetParentResourceName()}/${event}`, {
 
                 method: "post",
@@ -17,8 +13,6 @@ export const Nui = {
 
 
                         return await resp.json();
-
-                    return await resp.json();
 
         } catch (error) {
             throw Error(`Failed to fetch NUI callback ${event}! (${error})`);
