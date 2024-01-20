976e62424d82597a7fdf559a509744be76dd25ab
Gellipapa <Gellipapa@users.noreply.github.com>
2024-01-20 04:48:59 +0000

    :art:Code formatted in *.css *.js *.jsx files

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -18,8 +18,8 @@ import { createStore } from "solid-js/store";
 import { setLang } from "./Utils/Translate";
 
 function App() {
-                  const hudStorageState = useHudStorageState();
-      const { updateStatus, updateSpeedo, updateHud, changeVoiceRange } =
+  const hudStorageState = useHudStorageState();
+  const { updateStatus, updateSpeedo, updateHud, changeVoiceRange } =
     useHudStorageDispatch();
 
   const {
@@ -28,11 +28,11 @@ function App() {
     setDefaultConfigs,
     handleLocalStorage,
   } = useSettingsStorageDispatch();
-        const settingsStorageState = useSettingsStorageState();
+  const settingsStorageState = useSettingsStorageState();
   const settings = () => settingsStorageState.settings;
-            const showPanel = () => settingsStorageState.showPanel;
+  const showPanel = () => settingsStorageState.showPanel;
   //check update lang after render component
-        const [updateLang, setUpdateLang] = createSignal(false);
+  const [updateLang, setUpdateLang] = createSignal(false);
   const open = () => {
     document.body.style.display = "flex";
   };
@@ -43,8 +43,8 @@ function App() {
 
   createEffect(() => {
     window.addEventListener("message", function (event) {
-          const type = event.data.type;
-              const value = event.data.value;
+      const type = event.data.type;
+      const value = event.data.value;
       switch (type) {
         case "SHOW":
           value ? open() : close();
